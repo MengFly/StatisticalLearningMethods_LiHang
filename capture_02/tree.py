418abea1bd79dd63233cbddc627d345ac5e43a15
Mengfly <wangpengfei99@gmail.com>
2020-08-09 18:15:05 +0800

    添加查找数据点所属叶子节点方法 添加遍历节点下面所有的节点，找到最小距离的方法（可优化，应该不需要遍历所有的子节点）
    :待解决的问题
    怎么查找到最近临节点
    怎么查找到k最近临节点

--- capture_02/tree.py
+++ capture_02/tree.py
@@ -1,3 +1,5 @@
+from queue import Queue
+
 import numpy as np
 
 
@@ -7,23 +9,91 @@ class KDTree(object):
         # flatten elements, for next split data and build kdNode
         # 为每一个item添加index信息，方便后面对item进行查找的时候定位到在原始数组中的位置
         flatten_values = np.array([element.flatten() for element in np.array(values)])
-        self.index_values = list(enumerate(flatten_values))
+        index_values = list(enumerate(flatten_values))
 
         self.max_split_dim = np.shape(flatten_values)[-1]
-        self.root_node = self.build_node(self.index_values, None, 0)
+        self.root_node = self.__build_node(index_values, None, 0)
 
-    def build_node(self, values, parent_node, depth):
-        if len(values) == 0:
+    def __build_node(self, flatten_values, parent_node, depth):
+        if len(flatten_values) == 0:
             return None
-        medium_index = len(values) // 2
+        medium_index = len(flatten_values) // 2
         axis = depth % self.max_split_dim
-        values_sort = sorted(values, key=ContainsIndexItemgetter(axis))
+        values_sort = sorted(flatten_values, key=ContainsIndexItemgetter(axis))
         node_value = values_sort[medium_index]
         node = KDNode(node_value, axis, depth)
         node.parent_node = parent_node
-        node.left_node = self.build_node(values_sort[:medium_index], node, depth + 1)
-        node.right_node = self.build_node(values_sort[medium_index + 1:], node, depth + 1)
+        node.left_node = self.__build_node(values_sort[:medium_index], node, depth + 1)
+        node.right_node = self.__build_node(values_sort[medium_index + 1:], node, depth + 1)
         return node
+
+    def find_relation_leaf(self, x):
+        """
+        查找数据点x所属的叶子节点空间的分割叶子节点
+        :param x:数据点x
+        :return: 所属的叶子节点
+        """
+        if not x:
+            return None
+        if np.ndim(x) == 1:
+            flatten_values = np.array([x])
+        else:
+            flatten_values = np.array([element.flatten() for element in np.array(x)])
+        if self.max_split_dim == flatten_values.shape[-1]:
+            result = []
+            for item in flatten_values:
+                cur_node = self.root_node
+                while True:
+                    if cur_node.is_leaf():
+                        result.append(cur_node.value)
+                        break
+                    if cur_node.value[1][cur_node.axis] > item[cur_node.axis]:
+                        if cur_node.left_node is not None:
+                            cur_node = cur_node.left_node
+                        else:
+                            cur_node = cur_node.right_node
+                    else:
+                        if cur_node.right_node is not None:
+                            cur_node = cur_node.right_node
+                        else:
+                            cur_node = cur_node.left_node
+            return np.squeeze(result)
+        else:
+            raise KeyError(
+                "dim {} != {}, input dim must eq kdTree dim，".format(flatten_values.shape[-1], self.max_split_dim))
+
+    @staticmethod
+    def find_min_distance_node(node, x, method):
+        """
+        :param node: KDNode, need query node
+        :param x: query data point
+        :param method: distance method
+        :return: min distance and min distance node
+        """
+        nodes = Queue(maxsize=0)
+        nodes.put(node)
+        min_distance = None
+        min_distance_node = None
+        while not nodes.empty():
+            n = nodes.get()
+            distance = method(x, n.value)
+            if min_distance is None:
+                min_distance = distance
+                min_distance_node = n
+            else:
+                if min_distance > distance:
+                    min_distance = distance
+                    min_distance_node = n
+            if n.left_node is not None:
+                nodes.put(n.left_node)
+            elif n.right_node is not None:
+                nodes.put(n.right_node)
+
+        return min_distance, min_distance_node
+
+    @staticmethod
+    def euclidean_distance(x1, x2):
+        return np.sqrt(np.sum(np.square(x1, x2)))
 
     def __str__(self):
         return "KdTree->rootNode=\n" + str(self.root_node)
@@ -60,9 +130,9 @@ class KDNode(object):
         str_child_padding = " " * self.depth * 2
         self_str = "{}(axis={})".format(self.value, self.axis)
         if self.left_node:
-            self_str += "\n  {}|- Left : {}".format(str_child_padding, self.left_node)
+            self_str += "\n  {}|—— Left : {}".format(str_child_padding, self.left_node)
         if self.right_node:
-            self_str += "\n  {}|- Right : {}".format(str_child_padding, self.right_node)
+            self_str += "\n  {}|—— Right : {}".format(str_child_padding, self.right_node)
         return self_str
 
     def is_leaf(self):
@@ -76,7 +146,13 @@ if __name__ == '__main__':
     test_arr = ((2, 3, 6), (5, 4, 8), (9, 6, 1), (4, 7, 2), (8, 1, 5), (7, 2, 4))
     kd_tree = KDTree(test_arr)
     print(kd_tree)
+    print(kd_tree.find_relation_leaf(((1, 2, 3), (4, 5, 6), (9, 7, 1))))
 
     test_arr = ((2, 3), (5, 4), (9, 6), (4, 7), (8, 1), (7, 2))
     kd_tree = KDTree(test_arr)
     print(kd_tree)
+    print(kd_tree.find_relation_leaf(((1, 2), (3, 4))))
+
+    # test_arr = np.random.randn(20, 5, 1)
+    # kd_tree = KDTree(test_arr)
+    # print(kd_tree)
